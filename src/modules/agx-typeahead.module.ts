d620e2f5243cf940fe5047ddd951182d4cf23a73
Satit Riapit <siteslave@msn.com>
2018-11-13 00:09:25 +0700

    fixed httpClient module

--- src/modules/agx-typeahead.module.ts
+++ src/modules/agx-typeahead.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AgxTypeAheadComponent } from './agx-typeahead.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AgxTypeAheadComponent],
   exports: [AgxTypeAheadComponent, CommonModule],
-  imports: [CommonModule, HttpModule, JsonpModule],
+  imports: [CommonModule, HttpClientModule, JsonpModule],
   providers: []
 })
-export class AgxTypeaheadModule {}
+export class AgxTypeaheadModule { }
